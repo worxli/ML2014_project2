0e59f7daacd94937c0a9c531fd4e857897c0618d
Lukas Bischofberger <lukasbi@student.ethz.ch>
2014-11-16 13:42:45 +0100

    basic structure

--- main.m
+++ main.m
@@ -1,16 +1,40 @@
 clear;
 
 %% read data
-training = csvread('training.csv');
-testdata = csvread('testing.csv');
-validation = csvread('validation.csv');
+training = csvread('data/training.csv');
+testdata = csvread('data/testing.csv');
+validation = csvread('data/validation.csv');
+
+kfold = 10;
 
 %% 
+Xt = training;
+
+ind = crossvalind('Kfold', size(Xt,1), kfold);
+err = 0;
+
+% do kfold crossvalidation
+for i = 1:kfold
+    Xts = Xt(ind == i, 1:end-1);
+    Xtr = Xt(ind ~= i, 1:end-1);
+
+    Gts = Xt(ind == i, end);
+    Gtr = Xt(ind ~= i, end);
+    
+    %SVMStruct = svmtrain(Xtr,Gtr);
+    %SVMModel = fitcsvm(Xtr,Gtr,'KernelFunction','rbf','Standardize',true,'ClassNames',{'negClass','posClass'});
+    
+    curerr = 0;
+    
+    err = err + curerr;
+end
 
 
 
+return
 %% test on validation set
 
+preddata = 0;
 
 %% write to csv file for submission
 csvwrite('validationsetresult.csv', preddata);
